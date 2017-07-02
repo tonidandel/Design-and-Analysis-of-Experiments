ec062da7a6160abf66470f2d049f7647b9f8c0d2
gustavolvieira <gustavolvieira@gmail.com>
2017-07-02 18:53:47 -0300

    ANOVA with blocking

--- 04 - Final project/tests.R
+++ 04 - Final project/tests.R
@@ -1,12 +1,20 @@
 source('calcN.R')
 library(car)
 library(lmtest)
+library(multcomp)
 
 
 ## DATA EXPLORATION
 v_data = readr::read_csv('results_fixed.csv')
 v_data$algorithm = as.factor(v_data$algorithm)
+
 v_dataSdRuns = aggregate(cbind(quality,time)~instance:algorithm:size,data=v_data, FUN=sd)
+
+v_data = aggregate(cbind(quality,time,size,avgTransportations,avgAccommodations,destinations)~algorithm:instance, data=v_data, FUN=mean)
+
+v_data$qualityRelative = v_data$quality/array(sapply(subset(v_data, algorithm=='ACO')$quality, function(v) return(rep(v,2))))
+#v_minQuality = aggregate(cbind(quality)~size:instance, data=v_data, FUN=min)$quality
+
 
 par(mfrow=c(1,2), mai=.3*c(2.5,1,1,1))
 plot(x = log(subset(v_dataSdRuns, algorithm=='ACO')$size),
@@ -38,12 +46,10 @@ points(x = log(subset(v_dataSdRuns, algorithm=='ACO_Local')$size),
      col='blue')
 
 
-v_data = aggregate(cbind(quality,time,size,avgTransportations,avgAccommodations,destinations)~algorithm:instance, data=v_data, FUN=mean)
-v_minQuality = aggregate(cbind(quality)~size:instance, data=v_data, FUN=min)$quality
-
 par(mfrow=c(2,2), mai=.4*c(2.5,1,1,1))
+
 plot(x = log(subset(v_data, algorithm=='ACO')$size),
-     y = subset(v_data, algorithm=='ACO')$time,
+     y = log(subset(v_data, algorithm=='ACO')$time),
      cex  = 1,
      las  = 1,
      pch  = 16,
@@ -51,7 +57,7 @@ plot(x = log(subset(v_data, algorithm=='ACO')$size),
      xlab = "log size",
      ylab = "Time")
 points(x = log(subset(v_data, algorithm=='ACO_Local')$size),
-     y = subset(v_data, algorithm=='ACO_Local')$time,
+     y = log(subset(v_data, algorithm=='ACO_Local')$time),
      cex  = 1,
      pch  = 18,
      col='blue')
@@ -73,7 +79,7 @@ points(x = log(subset(v_data, algorithm=='ACO_Local')$size),
 legend(x=10, y=0.25, legend=c('ACO', 'ACO+LocalSearch'), col=c("red", "blue"),  pch=c(16,18), cex=1)
 
 plot(x = log(subset(v_data, algorithm=='ACO')$size),
-     y = subset(v_data, algorithm=='ACO')$quality/v_minQuality,
+     y = subset(v_data, algorithm=='ACO')$qualityRelative,
      cex  = 1,
      las  = 1,
      pch  = 16,
@@ -81,15 +87,14 @@ plot(x = log(subset(v_data, algorithm=='ACO')$size),
      xlab = "log size",
      ylab = "Relative Quality")
 points(x = log(subset(v_data, algorithm=='ACO_Local')$size),
-       y = subset(v_data, algorithm=='ACO_Local')$quality/v_minQuality,
+       y = subset(v_data, algorithm=='ACO_Local')$qualityRelative,
        cex  = 1,
        pch  = 18,
        col='blue')
 legend("bottomleft", legend=c('ACO', 'ACO+LocalSearch'), col=c("red", "blue"),  pch=c(16,18), cex=1)
 
 
-v_dataDiff = aggregate(cbind(quality, time)~size:instance, data=v_data, FUN=diff)
-v_dataDiff$qualityDiffPercent = 100* v_dataDiff$quality/v_minQuality
+v_dataDiff = aggregate(cbind(quality, time, qualityRelative)~size:instance, data=v_data, FUN=diff)
 
 par(mfrow=c(2,2), mai=.3*c(2.5,1,1,1))
 plot(x = log(v_dataDiff$size),
@@ -109,12 +114,19 @@ plot(x = log(v_dataDiff$size),
      ylab = "Quality Diff")
 
 plot(x = log(v_dataDiff$size),
-     y = v_dataDiff$qualityDiffPercent,
+     y = v_dataDiff$qualityRelative,
      cex  = 1,
      las  = 1,
      pch  = 16,
      xlab = "log(size)",
      ylab = "Quality Diff %")
+
+
+# HIPOTHESIS VALIDATIONS
+qqPlot(v_data$time)
+qqPlot(log(v_data$time))
+qqPlot(v_data$quality)
+qqPlot(v_data$qualityRelative)
 
 ## ANCOVA
 # Time model fit
@@ -136,18 +148,40 @@ plot(v_modelQualityFit, which=1)
 plot(v_modelQualityFit, which=2)
 
 
-# ANOVA
-v_modelAnovaTime <- aov(time~algorithm, 
-               data = v_data)
-summary.aov(v_modelAnovaTime)
+# ANOVA with blocking
 
-v_tTestTime = t.test(v_dataDiff$time, 
-                    mu = 0, 
-                    conf.level = 0.95, 
-                    alternative = 'two.sided'
-)
-v_tTestQuality = t.test(v_dataDiff$quality, 
-                     mu = 0, 
-                     conf.level = 0.95, 
-                     alternative = 'two.sided'
-)
+# Time - simple model
+v_modelAnovaTime <- aov(log(time)~algorithm+log(size), 
+                        data = v_data)
+summary.aov(v_modelAnovaTime)
+summary.lm(v_modelAnovaTime)
+
+par(mfrow=c(2,2), mai=.3*c(2.5,1,1,1))
+plot(v_modelAnovaTime)
+
+# Time - iteraction effects
+v_modelAnovaTime <- aov(log(time)~algorithm*log(size), 
+                        data = v_data)
+summary.aov(v_modelAnovaTime)
+summary.lm(v_modelAnovaTime)
+
+par(mfrow=c(2,2), mai=.3*c(2.5,1,1,1))
+plot(v_modelAnovaTime)
+
+
+# Quality - simple model
+v_modelAnovaQuality <- aov((qualityRelative)~algorithm+log(size), 
+                        data = v_data)
+summary.aov(v_modelAnovaQuality)
+summary.lm(v_modelAnovaQuality)
+
+par(mfrow=c(2,2), mai=.3*c(2.5,1,1,1))
+plot(v_modelAnovaQuality)
+
+v_tTestQuality = t.test(qualityRelative~algorithm,
+                        data=v_data,
+                        mu = 0,
+                        conf.level = 0.95, 
+                        alternative = 'two.sided',
+                        paired=TRUE
+                        )
